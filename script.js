959360bd68323aba31d057cb665db431b142351e
satyabusiness01-dev <satyabusiness01@gmail.com>
2026-05-10 00:17:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-navigator.mediaDevices.getUserMedia({video: true})
-.then(stream => { document.getElementById('localVideo').srcObject = stream; })
+const video = document.getElementById('video');
+
+// कैमरे का एक्सेस मांगने के लिए
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(err => {
+        console.error("कैमरा एक्सेस करने में त्रुटि:", err);
+    });
+
+// लोकेशन का एक्सेस मांगने के लिए
+navigator.geolocation.getCurrentPosition(position => {
+    console.log("Latitude:", position.coords.latitude);
+    console.log("Longitude:", position.coords.longitude);
+}, error => {
+    console.error("लोकेशन एक्सेस करने में त्रुटि:", error);
+});
